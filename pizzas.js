93caf2f9db8921d7c9f50a3e98861f03e1f1e9e0
magalhaeshelio <71614237+magalhaeshelio@users.noreply.github.com>
2021-01-28 19:25:52 +0100

    Add files via upload

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Pizza 1', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha'},
+    {id:2, name:'Pizza 2', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha'},
+    {id:3, name:'Pizza 3', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha'},
+    {id:4, name:'Pizza 4', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha'},
+    {id:5, name:'Pizza 5', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha'},
+    {id:6, name:'Pizza 6', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha'},
+    {id:7, name:'Pizza 7', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha'}
+];
